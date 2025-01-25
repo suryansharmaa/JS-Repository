22ba1aafc42a18a5cbae7e6d3dec85e18e69fa6c
Suryansh Sharma <suryansharmaa@gmail.com>
2025-01-25 16:36:54 +0530

    fetch

---
+++ 07. Other/fetching.js
@@ -0,0 +1,29 @@
+//  .text() method return promise
+// if resolved, it'll return text representation of the body
+
+// It will only show errors of networks nothing else
+
+// fetch("texttt.txt")
+//   .then((res) => res.text())
+//   .then((data) => console.log(data))
+//   .catch((error) => console.log(error));
+
+// Best way to do it
+fetch("text.txt")
+  .then((res) => {
+    if (!res.ok) throw Error(res.statusText);
+    return res.text();
+  })
+  .then((data) => console.log(data))
+  .catch((error) => console.log(error));
+
+//  Cool Stuff and using Async/Await
+const screen = document.querySelector(".webpage");
+
+async function fetcher() {
+  let response = await fetch("text.txt");
+  let result = await response.text();
+  screen.innerHTML = result;
+}
+
+fetcher();
